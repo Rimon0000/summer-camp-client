6c7c768d65b8587cd8511c5bc689b02a6749aa17
Your Name <Rimon0000>
2023-06-13 19:27:14 +0600

    add admin route and instructor route

---
+++ src/Routes/InstructorRoute.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
+import useInstructor from '../hooks/useInstructor';
+
+const InstructorRoute = ({children}) => {
+    const {user, loading} = useContext(AuthContext)
+    const [isInstructor, isInstructorLoading] = useInstructor()
+    const location = useLocation()
+
+    if(loading){
+        return <progress className="progress w-56"></progress>
+    }
+
+    if(user && isInstructor){
+        return children
+    }
+    return <Navigate to="/login" state={{ from: location}} replace></Navigate>;
+};
+
+export default InstructorRoute;
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
+
 import useAxiosSecure from './useAxiosSecure';
 import useAuth from './useAuth';
 
-
 const useCart = () =>{
     const {user, loading} = useAuth()
-    // const token = localStorage.getItem('access-token')
+    const token = localStorage.getItem('access-token')
     const [axiosSecure] = useAxiosSecure();
 
     const { refetch, data : cart = [] } = useQuery({
@@ -13,10 +13,19 @@ const useCart = () =>{
         // enabled:(!!user?.email && !!localStorage.getItem("access-token") , !loading),
 
         queryFn: async () =>{
-            const res = await axiosSecure(`/carts?email=${user?.email}`)
-            console.log('res from axios', res)
-            return res.data 
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            })
+            return res.json()
         },
+
+        // queryFn: async () =>{
+        //     const res = await axiosSecure(`/carts?email=${user?.email}`)
+        //     console.log('res from axios', res)
+        //     return res.data 
+        // },
       })
 
       return [cart, refetch]
